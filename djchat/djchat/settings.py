dbc59d2866133df3c405288874657fd8b3524e70
Jack-Chagarr <netamijr@gmail.com>
2024-06-09 21:10:00 +0900

    Add server app and models

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "account",
+    "server"
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "account.Account" #앱에서의 디폴트 유저 모델을 정의
