d9492a8a6b148dd3489a6f950da3db36bd68dd17
Jack-Chagarr <netamijr@gmail.com>
2024-06-23 18:56:14 +0900

    Add server list view sets

--- djchat/djchat/urls.py
+++ djchat/djchat/urls.py
@@ -15,9 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.routers import DefaultRouter
+from server.views import ServerListViewSet
+
+router = DefaultRouter()
+router.register("api/server/select", ServerListViewSet)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+
+] + router.urls
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, documment_root=settings.MEDIA_ROOT)
