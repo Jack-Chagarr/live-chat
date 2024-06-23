42657d4831cff6dc28bcac5011ad5d6c6763d577
Jack-Chagarr <netamijr@gmail.com>
2024-06-23 18:55:54 +0900

    Add drf_spectacular

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # External
+    "drf_spectacular",
     "rest_framework",
     # Internal
     "account",
@@ -141,4 +142,11 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
     ],
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Your Project API",
+    "DESCRIPTION": "Your project description",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
 }
